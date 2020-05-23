46ae274f2491ed48bf52322fcd1bbee441e97437
Petr Kahánek <kahanek.peta@gmail.com>
2020-05-23 10:20:20 +0200

    implemnted firebase utils, included ability to store authenticated users into apps' firebase

--- crown-clothing/src/firebase/firebase.utils.js
+++ crown-clothing/src/firebase/firebase.utils.js
@@ -13,6 +13,30 @@ const config = {
   appId: "1:1053939867007:web:b3fed15bbd3b1ba33a4e4a",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
