07f57703f38bea4533bf6dabd0bbee452c077905
Petr Kahánek <kahanek.peta@gmail.com>
2020-05-29 11:11:42 +0200

    added addItem and removeItem funcitonality to checkout item component

--- crown-clothing/src/redux/cart/cart.reducer.js
+++ crown-clothing/src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,6 +17,18 @@ const cartReducer = (currentState = INITIAL_STATE, action) => {
       return {
         ...currentState,
         cartItems: addItemToCart(currentState.cartItems, action.payload),
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...currentState,
+        cartItems: currentState.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...currentState,
+        cartItems: removeItemFromCart(currentState.cartItems, action.payload),
       };
     default:
       return currentState;
